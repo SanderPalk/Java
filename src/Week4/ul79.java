466d5db8f053509547bd01b0603b959c9ce79327
SPalk <95194970+SanderPalk@users.noreply.github.com>
2022-09-15 08:38:35 +0300

    Ul79 and repository configuration

--- src/Week4/ul79.java
+++ src/Week4/ul79.java
@@ -1,8 +1,19 @@
 package Week4;
+import java.util.Scanner;
 
 public class ul79 {
     public static void main(String[] args) {
         NumberStatistics stats = new NumberStatistics();
+        System.out.println("Type numbers: ");
+        Scanner reader = new Scanner(System.in);
+        int number = reader.nextInt();
+        while (true) {
+            number = reader.nextInt();
+            if (number == -1) {
+                break;
+            }
+            stats.addNumber(number);
+        }
         stats.addNumber(3);
         stats.addNumber(5);
         stats.addNumber(1);
