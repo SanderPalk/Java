366572c039e2cfa28e2481c337da089688b37e3d
Sander <sanderpalk0@gmail.com>
2022-11-08 10:13:40 +0200

    Exercise: 7 Week 7 DONE

---
+++ src/Week7/ul7.java
@@ -0,0 +1,18 @@
+package Week7;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class ul7 {
+    public static void main(String[] args) {
+        Dictionary dictionary = new Dictionary();
+        dictionary.add("apina", "monkey");
+        dictionary.add("banaani", "banana");
+        dictionary.add("cembalo", "harpsichord");
+
+        ArrayList<String> translations = dictionary.translationList();
+        for(String translation: translations) {
+            System.out.println(translation);
+        }
+    }
+}
